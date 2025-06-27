9892b9d3a6b586ebfecfa8c57d4c46abaa134c47
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-06-27 23:06:24 +0530

    n

--- functions/ASYNC/async.js
+++ functions/ASYNC/async.js
@@ -1,6 +1,14 @@
 // example javascript
-
+console.log("india")
+console.log("delhi")
 function hello(){
     console.log("hello")
 }
 setTimeout(hello,2000)
+// only setTimeout function
+
+setTimeout(()=>{
+console.log("hello World")
+},4000);
+console.log("is wait")
+console.log("not wait")

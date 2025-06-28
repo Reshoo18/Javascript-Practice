0a2cce81b2bc5aac0f6b4f876e3b4e874c819be4
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-06-28 11:37:56 +0530

    new code

--- functions/ASYNC/callBackHell.js
+++ functions/ASYNC/callBackHell.js
@@ -6,8 +6,11 @@ function getData(dataId,getNextData){
         }
     },2000)
 }
+console.log("getting data 1")
 getData(1,()=>{
+    console.log("gettng data 2");
     getData(2,()=>{
+        console.log("gettng data 3");
         getData(3);
     })
     })

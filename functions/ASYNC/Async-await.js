8190b3c20297bfc0c7d7b885d788011d1967a2cb
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-06-29 12:49:34 +0530

    neww

--- functions/ASYNC/Async-await.js
+++ functions/ASYNC/Async-await.js
@@ -4,22 +4,51 @@
 
 
 //1:- Acync  :- means its return promise 
- async function hello (){
-    console.log("hello ")
- }
- hello();
+//  async function hello (){
+//     console.log("hello ")
+//  }
+//  hello();
 
  //2 await :- We cam use await only with in the asuync function only not in individually function 
-  function api(){
+//   function api(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             console.log("the data is here")
+//             resolve(300)
+//         },2000)
+//     })
+//   }
+//   calling await 
+// async function getData(){
+//     await api();
+// }
+// getData();
+
+
+
+
+
+
+
+
+//ASYNC AWAIT WHOLE CODE
+function getData(dataId){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            console.log("the data is here")
-            resolve(300)
+            console.log("data",dataId);
+            resolve("success")
         },2000)
     })
-  }
-//   calling await 
-async function getData(){
-    await api();
 }
-getData();
+
+async function getNewCall(){
+    await getData(1);
+    await getData(2);
+    await getData(3);
+    await getData(4);
+    await getData(5);
+    await getData(6);
+}
+    
+
+getNewCall();

ccf9a93fe4df4fa016b53ef61fc3c0e96cfda4b3
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-06-30 12:14:46 +0530

    new

---
+++ functions/EVENTS/events.js
@@ -0,0 +1,17 @@
+let btn1=document.querySelector("#btn1")
+btn1.onclick=()=>{
+console.log("button is cliced")
+let a=25;
+a++;
+console.log(a);
+}
+
+
+let div=document.querySelector("div")
+div.onmouseover=()=>{
+    console.log("this is a box")
+}
+let btn2=document.querySelector("#btn2")
+btn2.ondblclick=()=>{
+    console.log("hello i m button 2")
+}

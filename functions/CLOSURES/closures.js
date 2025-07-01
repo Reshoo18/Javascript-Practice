bc42a10cd4e326aaaed204202c0c7042242f1fc6
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-07-01 13:40:26 +0530

    new

--- functions/CLOSURES/closures.js
+++ functions/CLOSURES/closures.js
@@ -1,11 +1,24 @@
 //const name ="hello"
-function Outer(){
-    //let name="reshoo"
-    function inner(){
+// function Outer(){
+//     //let name="reshoo"
+//     function inner(){
        
-       let name="ranjan"
+//        let name="ranjan"
+//         console.log(name)
+//     }
+//     inner();
+// }
+// Outer();
+
+
+//CLOSURE CODE EXUCUTION
+function Outer(){
+    let name ="reshoo"
+    function innerFUnc(){
         console.log(name)
     }
-    inner();
+    return innerFUnc
 }
-Outer();
+let inner =Outer();
+
+inner();
